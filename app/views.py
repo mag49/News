59794cd77c652eff194e9b69e3f642e619c5eba0
Ngethe <ciru930@gmail.com>
2020-02-03 17:53:24 +0300

    created the views.py file

--- app/views.py
+++ app/views.py
@@ -8,4 +8,5 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+     message = 'Welcome to news app'
+    return render_template('index.html',message = message)
