39f684015485c95ce59c1345bbab1f0246b5afd3
Ngethe <ciru930@gmail.com>
2020-02-04 12:34:04 +0300

    added class for the news source in the models folder

---
+++ app/models/news.py
@@ -0,0 +1,13 @@
+class News:
+    '''
+    News class to define News Objects
+    '''
+
+    def __init__(self,id,title,content,image,author,date, url):
+        self.id =id
+        self.title = title
+        self. content= content
+        self.image = image
+        self.author = author
+        self.date = date
+        self.url = url
---
+++ app/models/source.py
@@ -0,0 +1,12 @@
+class Source:
+    '''
+    all source class to define news objects
+    '''
+    def __init__(self, name, author, url,content, country,category, id):
+        self.name = name
+        self.author = author
+        self.url = url
+        self.description = description
+        self.country = country
+        self.category = category
+        self.id = id
