90af70ef40a0f160612fc325a97e529a9b37ac40
Ngethe <ciru930@gmail.com>
2020-02-04 10:19:29 +0300

    added content to the body of index.html

--- app/views.py
+++ app/views.py
@@ -10,3 +10,12 @@ def news(news_id):
     '''
      
     return render_template('news.html',id = news_id)
+
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    title = 'Home - Welcome to The best News Website Online'
+    return render_template('index.html', title = title)    
